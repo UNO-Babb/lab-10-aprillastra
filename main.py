89a79e3e651bbbf6abe82b5c6f3697c55f830e8e
aprillastra <klastra-bernal@unomaha.edu>
2025-04-21 04:55:19 +0000

    lab 10

--- main.py
+++ main.py
@@ -2,3 +2,42 @@
 #Name:
 #Date:
 #Assignment:
+
+import food
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+report = food.get_report()
+
+names = []
+calories = []
+protein = []
+
+for item in report: 
+    try: 
+        name = item["Description"]
+        cal = item["Data"]["Calories"]
+        prot = item["Data"]["Protein"]
+
+        names.append(name)
+        calories.append(cal)
+        protein.append(prot)
+    except:
+        pass # Skip foods with missing data 
+
+df = pd.DataFrame({
+    "Food": names,
+    "Calories": calories, 
+    "Protein": protein
+})
+
+for i in range(len(df)):
+    plt.text(df["Calories"][i]+ 1, df["Protein"[i], df["Food"][i]])
+
+
+plt.scatter(df["Calories"], df["Protein"][i], df["Food"][i])
+
+plt.title("Calories vs Proteins")
+
+plt.savefig("food.png")
+
